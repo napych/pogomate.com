0fc37ce53e8905d8e1ebd1e29249b95c687b69ad
Eduard Kutuev <nap@napych.com>
2020-07-18 05:17:59 +0300

    Added Gamepress Greater League pokemon.

--- src/Data/Gamepress/GL.php
+++ src/Data/Gamepress/GL.php
@@ -2,43 +2,46 @@
 
 namespace Pogo\Data\Gamepress {
 
+    use Pogo\Data\Pokemon;
+
     class GL
     {
         const DESCRIPTION = 'GamePress Greater League';
         const RANKS = [
-            'Tier 1' => ['Azumarill', 'Skarmory', 'Altaria', 'Registeel'],
+            'Tier 1' => [Pokemon::AZUMARILL, Pokemon::SKARMORY, Pokemon::ALTARIA, Pokemon::REGISTEEL],
             'Tier 1.5' => [
-                'Venusaur',
-                'Umbreon',
-                'Deoxys',
-                'Probopass',
-                'Bastiodon',
-                'Tropius',
-                'Sableye',
-                'Swampert',
-                'Medicham',
-                'Stunfisk',
-                'Hypno',
-                'Meganium'
+                Pokemon::VENUSAUR,
+                Pokemon::UMBREON,
+                Pokemon::DEOXYS,
+                Pokemon::PROBOPASS,
+                Pokemon::BASTIODON,
+                Pokemon::TROPIUS,
+                Pokemon::SABLEYE,
+                Pokemon::SWAMPERT,
+                Pokemon::MEDICHAM,
+                Pokemon::STUNFISK,
+                Pokemon::HYPNO,
+                Pokemon::MEGANIUM
             ],
             'Tier 2' => [
-                'Stunfisk',
-                'Melmetal',
-                'Haunter',
-                'Raichu',
-                'Marowak',
-                'Cresselia',
-                'Quagsire',
-                'Toxicroak',
-                'Jumpluff',
-                'Jirachi',
-                'Mew',
-                'Whiscash',
-                'Dewdong',
-                'Vigoroth',
-                'Shiftry',
-                'Lanturn',
-                'Lapras'
+                Pokemon::ZWELIOUS,
+                Pokemon::STUNFISK,
+                Pokemon::MELMETAL,
+                Pokemon::HAUNTER,
+                Pokemon::RAICHU,
+                Pokemon::MAROWAK,
+                Pokemon::CRESSELIA,
+                Pokemon::QUAGSIRE,
+                Pokemon::TOXICROAK,
+                Pokemon::JUMPLUFF,
+                Pokemon::JIRACHI,
+                Pokemon::MEW,
+                Pokemon::WHISCASH,
+                Pokemon::DEWGONG,
+                Pokemon::VIGOROTH,
+                Pokemon::SHIFTRY,
+                Pokemon::LANTURN,
+                Pokemon::LAPRAS
             ]
         ];
     }
