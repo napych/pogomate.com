e85e3481441e735d63d903089553dff3fc778524
Eduard Kutuev <nap@napych.com>
2020-08-03 21:25:40 +0300

    Added stopPropagation for copy/reveal/conceal buttons.

--- js/main/snippet-string.js
+++ js/main/snippet-string.js
@@ -1,5 +1,6 @@
 function snippetStringCopy(event) {
     clipboard.writeText($(event.target).closest('.string').data('string'));
+    event.preventDefault();
 }
 
 function snippetStringReveal(event) {
@@ -7,12 +8,14 @@ function snippetStringReveal(event) {
     btn.replaceWith(
         $('<span class="string-revealed"><a href="#" class="string-conceal">Conceal</a><br><p>' + btn.closest('.string').data('string') + '</p></span>')
     );
+    event.preventDefault();
 }
 
 function snippetStringConceal(event) {
     $(event.target).closest('.string-revealed').replaceWith(
         $('<a href="#" class="string-reveal">Reveal</a>')
     );
+    event.preventDefault();
 }
 
 $(document).on('click dblclick touchend', '.string .string-copy', snippetStringCopy);
