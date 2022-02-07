f3cd9c29510d79229784de8a6ce38691db773305
Eduard Kutuev <ed@napych.com>
2022-02-07 21:07:28 +0300

    Added larger expires header for moves (testing).

--- controllers/move.php
+++ controllers/move.php
@@ -12,7 +12,7 @@ class Move extends Controller
 {
     protected function indexAction(AnyString $link)
     {
-        View::addExpires(60 * 60 * 24 * 7);
+        View::addExpires(31536000); // 1y
         $moves = \Pogo\Pokemon\Move::getByLink($link->val());
         if (empty($moves)) {
             throw new HttpError(404);
